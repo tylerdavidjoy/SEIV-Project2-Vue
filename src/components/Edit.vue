d6f6faa28929a23ec0e50523901e80288db1194a
CamWMcDaniel <cameron.mcdaniel@eagles.oc.edu>
2020-09-23 08:37:59 -0500

    Made courses global variable, form items in divs

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -2,80 +2,92 @@
   <div>
     <h1 style="font-size:60px">Edit Course</h1>
     <div>
-      <p>Number:</p>
-      <input v-model="cNumber" placeholder="Number" />
-
-      <p>Name:</p>
-      <input v-model="cName" placeholder="Name" />
-
-      <p>Professor:</p>
-      <input v-model="cProfessor" placeholder="Professor" />
-
-      <p>Semester:</p>
-      <input v-model="cSemester" placeholder="Semester" />
-
-      <p>Credit:</p>
-      <input v-model="cCredit" placeholder="Credit" />
-
-      <p>Start Time:</p>
-      <input v-model="cStartTime" placeholder="Start Time" />
-
-      <p>End Time:</p>
-      <input v-model="cEndTime" placeholder="End Time" />
-
-      <p>Room:</p>
-      <input v-model="cRoom" placeholder="Room" />
-
-      <p>Description:</p>
-      <input v-model="cDescription" placeholder="Description" />
-
-      <p>Department:</p>
-      <input v-model="cDepartment" placeholder="Department" />
-
-      <p>Level:</p>
-      <input v-model="cLevel" placeholder="Level" />
-
-      <button v-on:click="cSave()">Save</button>
-      <button v-on:click="backToList()">Cancel</button>
-      <button v-on:click="cDelete()">Delete</button>
+      <div>
+        <label for="cNumber">Number:</label>
+        <input id="cNumber" v-bind="cNumber" placeholder="Number" />
+      </div>
+      <div>
+        <label for="cName">Name:</label>
+        <input id="cName" v-bind="cName" placeholder="Name" />
+      </div>
+      <div>
+        <label for="cProfessor">Professor:</label>
+        <input id="cProfessor" v-bind="cProfessor" placeholder="Professor" />
+      </div>
+      <div>
+        <label for="cSemester">Semester:</label>
+        <input id="cSemester" v-bind="cSemester" placeholder="Semester" />
+      </div>
+      <div>
+        <label for="cCredit">Credit:</label>
+        <input id="cCredit" v-bind="cCredit" placeholder="Credit" />
+      </div>
+      <div>
+        <label for="cStartTime">Start Time:</label>
+        <input id="cStartTime" v-bind="cStartTime" placeholder="Start Time" />
+      </div>
+      <div>
+        <label for="cEndTime">End Time:</label>
+        <input id="cEndTime" v-bind="cEndTime" placeholder="End Time" />
+      </div>
+      <div>
+        <label for="cRoom">Room:</label>
+        <input id="cRoom" v-bind="cRoom" placeholder="Room" />
+      </div>
+      <div>
+        <label for="cDescription">Description:</label>
+        <input id="cDescription" v-bind="cDescription" placeholder="Description" />
+      </div>
+      <div>
+        <label for="cDepartment">Department:</label>
+        <input id="cDepartment" v-bind="cDepartment" placeholder="Department" />
+      </div>
+      <div>
+        <label for="cLevel">Level:</label>
+        <input id="cLevel" v-bind="cLevel" placeholder="Level" />
+      </div>
+      <div>
+        <button v-on:click="cSave()">Save</button>
+        <button v-on:click="backToList()">Cancel</button>
+        <button v-on:click="cDelete()">Delete</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Edit",
-  data() {
-    return {
-      cNumber: "",
-      cName: "",
-      cProfessor: "",
-      cSemester: "",
-      cCredit: "",
-      cStartTime: "",
-      cEndTime: "",
-      cRoom: "",
-      cDescription: "",
-      cDepartment: "",
-      cLevel: ""
-    };
+  data(){
+    return{
+        cNumber: "",
+        cName: "",
+        cProfessor: "",
+        cSemester: "",
+        cCredit: "",
+        cStartTime: "",
+        cEndTime: "",
+        cRoom: "",
+        cDescription: "",
+        cDepartment: "",
+        cLevel: ""
+    }
   },
   methods: {
-        cSave: function(){
-            // Save the course
+    cSave: function() {
+      // Save the course
 
-            // Go back to select page
-            this.backToList();
-          },
-        cDelete: function(){
-            // Delete the course
+      // Go back to select page
+      this.backToList();
+    },
+    cDelete: function() {
+      // Delete the course
 
-            // Go back to select page
-            this.backToList();
-          },
-        backToList: function(){
-          this.$router.push('/listview');
-        }
-      }
+      // Go back to select page
+      this.backToList();
+    },
+    backToList: function() {
+      this.$router.push("/listview");
+    },
+  },
 };
 </script>
