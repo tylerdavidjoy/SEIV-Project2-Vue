a3396f21c6a980053e5b0c6d1f41d8a90cb7d8b7
CamWMcDaniel <cameron.mcdaniel@eagles.oc.edu>
2020-09-23 23:05:43 -0500

    fixed data import with power of two brains

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,77 +1,130 @@
 <template>
   <div>
-    <h1 v-if="$route.params.new != true" style="font-size:60px">Edit Course {{ $route.params.id }}</h1>
+    <h1 v-if="$route.params.new != true" style="font-size:60px">
+      Edit Course {{ $route.params.id }}
+    </h1>
     <h1 v-else style="font-size:60px">Add Course</h1>
     <div>
       <div>
-        <label for="cNumber">Number:</label>
-        <input id="cNumber" v-bind="course.Course_Number" value:course.Course_Number placeholder="Number" />
+        <label>Number:</label>
+        <input
+          type="text"
+          v-model="course.Course_Number"
+          placeholder="Number"
+        />
       </div>
       <div>
-        <label for="cName">Name:</label>
-        <input id="cName" v-bind="course.Course_Name" value:course.Course_Name placeholder="Name" />
+        <label>Name:</label>
+        <input
+          type="text"
+          v-model="course.Course_Name"
+          placeholder="Name"
+        />
       </div>
       <div>
-        <label for="cProfessor">Professor:</label>
-        <input id="cProfessor" v-bind="course.Course_Professor_Full_Name" value:course.Course_Course_Professor_Full_Name placeholder="Professor" />
+        <label>Professor:</label>
+        <input
+          type="text"
+          v-model="course.Course_Professor_Full_Name"
+          placeholder="Professor"
+        />
       </div>
       <div>
-        <label for="cSemester">Semester:</label>
-        <input id="cSemester" v-bind="course.Course_Semester" value:course.Course_Semester placeholder="Semester" />
+        <label>Semester:</label>
+        <input
+          type="text"
+          v-model="course.Course_Semester"
+          placeholder="Semester"
+        />
       </div>
       <div>
-        <label for="cCredit">Credit:</label>
-        <input id="cCredit" v-bind="course.Course_Credit" value:course.Course_Credit placeholder="Credit" />
+        <label>Credit:</label>
+        <input
+          type="text"
+          v-model="course.Course_Credit"
+          placeholder="Credit"
+        />
       </div>
       <div>
-        <label for="cStartTime">Start Time:</label>
-        <input id="cStartTime" v-bind="course.Course_Start_Time" value:course.Course_Start_Time placeholder="Start Time" />
+        <label>Start Time:</label>
+        <input
+          type="text"
+          v-model="course.Course_Start_Time"
+          placeholder="Start Time"
+        />
       </div>
       <div>
-        <label for="cEndTime">End Time:</label>
-        <input id="cEndTime" v-bind="course.Course_End_Time" value:course.Course_End_Start placeholder="End Time" />
+        <label>End Time:</label>
+        <input
+          type="text"
+          v-model="course.Course_End_Time"
+          placeholder="End Time"
+        />
       </div>
       <div>
-        <label for="cRoom">Room:</label>
-        <input id="cRoom" v-bind="course.Course_Room" value:course.Course_Room placeholder="Room" />
+        <label>Room:</label>
+        <input
+          type="text"
+          v-model="course.Course_Room"
+          placeholder="Room"
+        />
       </div>
       <div>
-        <label for="cDescription">Description:</label>
-        <input id="cDescription" v-bind="course.Course_Description" value:course.Course_Description placeholder="Description" />
+        <label>Description:</label>
+        <input
+          type="text"
+          v-model="course.Course_Description"
+          placeholder="Description"
+        />
       </div>
       <div>
-        <label for="cDepartment">Department:</label>
-        <input id="cDepartment" v-bind="course.Course_Department" value:course.Course_Department placeholder="Department" />
+        <label>Department:</label>
+        <input
+          type="text"
+          v-model="course.Course_Department"
+          placeholder="Department"
+        />
       </div>
       <div>
-        <label for="cLevel">Level:</label>
-        <input id="cLevel" v-bind="course.Course_Level" value:course.Course_Level placeholder="Level" />
+        <label>Level:</label>
+        <input
+          type="text"
+          v-model="course.Course_Level"
+          placeholder="Level"
+        />
       </div>
       <div>
         <button v-on:click="cSave($route.params.new)">Save</button>
         <button v-on:click="backToList()">Cancel</button>
-        <button v-on:click="cDelete($route.params.new)" v-if="$route.params.new != true">Delete</button>
+        <button
+          v-on:click="cDelete($route.params.new)"
+          v-if="$route.params.new != true"
+        >
+          Delete
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   data(){
     return{
         course: {
-          Course_Credit: ​​​"",
-          Course_Department: "",​
-          Course_Description: "",  ​​​
-          Course_End_Time: "",  ​​​
-          Course_Level: "", ​​​
-          Course_Name: "",​​​
-          Course_Number: "",​​
-          Course_Professor_Full_Name: "",  ​​​
-          Course_Room: "",  ​​​
-          Course_Semester: "",
+          Course_Number: "",
+          Course_Name: "",
+          Course_Professor_Full_Name: "",
+          Course_Credit: "",
+          Course_Department: "",
+          Course_Description: "",
+          Course_End_Time: "",
           Course_Start_Time: "",
+          Course_Semester: "",
+          Course_Level: "",
+          Course_Room: ""
         }
     }
   },
@@ -113,7 +166,7 @@ export default {
       .get("http://team2.eaglesoftwareteam.com/courses/" + this.$router.params.id)
       .then(response => {
         console.log(response.data)
-        this.courses = response.data;
+        this.course = response.data;
       })
       .catch(error => {
         console.log("ERROR: " + error.response)
