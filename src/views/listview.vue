13801eb62459c5798de3de3c777d23bed908709a
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2020-09-23 20:26:17 -0500

    Adding API calls

--- src/views/listview.vue
+++ src/views/listview.vue
@@ -6,11 +6,21 @@
 
 <script>
 import Courses from "@/components/Courses.vue";
-
+import axios from 'axios'
 export default {
   name: "listview",
   components: {
     Courses
+  },
+  created() {
+    axios
+    .get("http://team2.eaglesoftwareteam.com/courses")
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log("ERROR: " + error.response)
+    })
   }
 };
 </script>
