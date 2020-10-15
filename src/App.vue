4f54662483a748b3e89889bf03de3b4eb23bd3f4
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2020-10-14 21:31:50 -0500

    Added log out functionality and button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-
+    <meta name="google-signin-client_id" content="761915308223-ua9pjnk2765b2qd88dda10htmhlu64js.apps.googleusercontent.com">
+    <div id="nav" class="nav">
+      <button v-if="$route.name != 'Login'" v-on:click="signOut()" class="btn btn-dark btn-lg" style="margin-left: 90%;" >Log Out</button>
     </div>
     <router-view />
   </div>
 </template>
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<script>
+export default {
+      methods: {
+      signOut: function() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+            });
+        
+        window.location.href = 'http://localhost:8080/';
+      }
+    }
+}
+</script>
 
 <style>
 #app {
@@ -18,6 +36,8 @@
 
 #nav {
   padding: 30px;
+  background-color: #00D0DD;
+  text-align: right;
 }
 
 #nav a {
