a8870e0375ea92747c7149d36c47660a3cfb7543
CamWMcDaniel <cameron.mcdaniel@eagles.oc.edu>
2020-10-21 21:09:20 -0500

    student profile shows data and links to plan

---
+++ src/views/profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <Profile />
+  </div>
+</template>
+
+<script>
+import Profile from "@/components/Profile.vue";
+
+export default {
+  name: "profile",
+  components: {
+    Profile
+  }
+};
+</script>
