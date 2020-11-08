c317aff370dbf040c6b547e1ce9e856694d4fe1e
Tyler Joy <tyler.david.joy@eagles.oc.edu>
2020-11-08 13:10:35 -0600

    Removed work arround

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ window.onSignIn = function (googleUser) {
 
 //      userData = {user_id:userID,role_type:role, role_id: roleID, plan_id:planID};
   
-/*var userdata = getUser(profile.getEmail()); //Ask backend for user data UNCOMMENT WHEN BACKEND IS FIXED
+var userdata = getUser(profile.getEmail()); //Ask backend for user data UNCOMMENT WHEN BACKEND IS FIXED
 
   window.user = {
     id: profile.getId(),
@@ -31,14 +31,9 @@ window.onSignIn = function (googleUser) {
     role_type: userdata.role_type,
     role_id: userdata.role_id,
     plan_id: userdata.plan_id
-  }; */
+  }; 
 
-  window.user = {
-    id: profile.getId(),
-    name: profile.getName(),
-    imgUrl: profile.getImageUrl(),
-    email: profile.getEmail(),
-  };
+
 
   console.log(window.user);
 };
